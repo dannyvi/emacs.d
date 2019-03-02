c97ed8d4fed590387e5a0a980f4c70ddf2d23c43
dannyvi <dannyvis@icloud.com>
2019-03-02 11:00:49 +0800

    with documentation

---
+++ README.md
@@ -0,0 +1,30 @@
+Use Emacs 
+==========
+
+How can we speed up programming by using a complete interactive editor?
+
+Interface
+----------
+
+Theme
+----------
+
+Compile
+----------
+
+Debug
+----------
+
+Shortcuts
+----------
+
+Macro
+----------
+
+VCS
+----------
+
+License
+----------
+
+**GPL3**
--- init.el
+++ init.el
@@ -3,10 +3,10 @@
       (setq initial-frame-alist
             '(
               (tool-bar-lines . 0)
-              (width . 80) ; chars
+              (width . 84) ; chars
               (height . 48) ; lines
               (background-color . "honeydew")
-              (left . 780)
+              (left . 770)
               (top . 10)))))
 
 (require 'package)
@@ -14,6 +14,9 @@
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
+(setq custom-safe-themes t)
+(load-theme 'light-Q)
 
 (setq url-gateway-method 'socks)
 (setq socks-server '("Default server" "127.0.0.1" 1080 5))
@@ -70,59 +73,25 @@
   (evil-define-key 'normal global-map (kbd "TAB") 'indent-for-tab-command)
 )
 
-(use-package magit
-;;  :custom-face
-;;  (magit-branch-local ((t (:foreground "#fa88ca"))))
-;;  (magit-branch-remote ((t (:foreground "#89ca88"))))
-;;  (magit-process-ng ((t (:inherit magit-section-heading :foreground "#f090c0"))))
-;;  (magit-process-ok ((t (:inherit magit-section-heading :foreground "#72e090"))))
-;;  (magit-section-heading ((t (:foreground "#f0d270" :weight bold))))
-;;  (magit-section-highlight ((t (:background "#f9f9f9"))))
-  )
+(use-package magit)
 
-(use-package counsel
-  :config (ivy-mode 1)
-;;  :custom-face
-;;  (ivy-current-match ((t (:background "#8a99d7" :foreground "white"))))
-;;  (ivy-cursor ((t (:background "#caeade" :foreground "white"))))
-;;  (ivy-minibuffer-match-face-2 ((t (:background "#f9ccda" :foreground "#8a8aba"))))
-  )
-;(require 'dashboard)
-;(dashboard-setup-startup-hook)
+(use-package counsel :config (ivy-mode 1))
+
 (use-package dashboard
   :init
   (setq dashboard-banner-logo-title "EEEEEEEEEEEEEEmacs")
   (setq dashboard-items '((recents . 10) (bookmarks . 10)))
   :config  (dashboard-setup-startup-hook)
   (define-key dashboard-mode-map (kbd "<mouse-1>") 'widget-button-press))
-;;  :config
-;;  (dashboard-setup-startup-hook)
-;;  :bind (:map dashboard-mode-map ("<mouse-1>" . widget-button-press))
-;;  )
 
-(use-package rainbow-delimiters
-;;  :custom-face
-;;  (rainbow-delimiters-depth-1-face ((t (:foreground "#eeeef8"))))
-;;  (rainbow-delimiters-depth-2-face ((t (:foreground "#e3e3f2"))))
-;;  (rainbow-delimiters-depth-3-face ((t (:foreground "#dedee8"))))
-;;  (rainbow-delimiters-depth-4-face ((t (:foreground "#d8d8e0"))))
-;;  (rainbow-delimiters-depth-5-face ((t (:foreground "#d0d0d8"))))
-;;  (rainbow-delimiters-depth-6-face ((t (:foreground "#c8c8d0"))))
-;;  (rainbow-delimiters-depth-7-face ((t (:foreground "#c0c0c9"))))
-;;  (rainbow-delimiters-depth-8-face ((t (:foreground "#b8b8c0"))))
-;;  (rainbow-delimiters-depth-9-face ((t (:foreground "#a8a8b0"))))
-;;  (rainbow-delimiters-unmatched-face ((t (:foreground "#f4fff8" :background "#ff9999"))))
-  )
+(use-package rainbow-delimiters)
 
-(use-package rainbow-blocks
-  )
+(use-package rainbow-blocks)
 
 (use-package racket-mode
   :init (setq racket-racket-program "racket")
   :hook ((racket-mode . paredit-mode)
 	 (racket-mode . rainbow-delimiters-mode))
-;  :custom-face
-;  (racket-selfeval-face ((t (:foreground "#cfa9d6"))))
   )
 
 (use-package paredit
@@ -142,10 +111,18 @@
  (setq pdf-annot-activate-created-annotations t)
  ;; use normal isearch
  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
- (define-key pdf-view-mode-map (kbd "<double-wheel-right>") 'image-forward-hscroll)
- (define-key pdf-view-mode-map (kbd "<double-wheel-left>") 'image-backward-hscroll)
-
+ (define-key pdf-view-mode-map
+   (kbd "<double-wheel-right>") 'image-forward-hscroll)
+ (define-key pdf-view-mode-map
+   (kbd "<double-wheel-left>") 'image-backward-hscroll)
  )
+
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -160,71 +137,9 @@
     (pdf-tools paredit-mode racket-mode exec-path-from-shell use-package))))
 
 (setq ansi-color-names-vector
-      ["#888888" "#ff6699" "green" "#ffff22" "#403ab0" "magenta" "#70e3d0" "#f9f9f9"])
+      ["#888888" "#ff6699" "green" "#ffff22"
+       "#403ab0" "magenta" "#70e3d0" "#f9f9f9"])
 (setq ansi-color-map (ansi-color-make-color-map))
-
-;;(custom-set-faces
-;; ;; custom-set-faces was added by Custom.
-;; ;; If you edit it by hand, you could mess it up, so be careful.
-;; ;; Your init file should contain only one such instance.
-;; ;; If there is more than one, they won't work right.
-;; '(default ((t (:foreground "#a3a3a3" :background "#fcfcfc" :height 120 :family "Menlo"))))
-;; '(bold ((t (:foreground "#faaaaf"))))
-;; '(cursor ((t (:background "#aceab9"))))
-;; '(error ((t (:foreground "#f95a7a"))))
-;; '(font-lock-builtin-face ((t (:foreground "#e0c293"))))
-;; '(font-lock-comment-delimiter-face ((t (:foreground "#cfcfcf"))))
-;; '(font-lock-comment-face ((t (:foreground "#d8d8d8"))))
-;; '(font-lock-constant-face ((t (:foreground "#c8a3e0"))))
-;; '(font-lock-doc-face ((t (:foreground "#afd4d4"))))
-;; '(font-lock-function-name-face ((t (:foreground "#efcf9f"))))
-;; '(font-lock-keyword-face ((t (:foreground "#fc9faf"))))
-;; '(font-lock-negation-char-face ((t (:foreground "#d89999"))))
-;; '(font-lock-preprocessor-face ((t (:foreground "#bf3fbf"))))
-;; '(font-lock-regexp-grouping-backslash ((t (:foreground "#f99999"))))
-;; '(font-lock-regexp-grouping-construct ((t (:foreground "#aaa999"))))
-;; '(font-lock-string-face ((t (:foreground "#7fbf8f"))))
-;; '(font-lock-type-face ((t (:foreground "#99a3e9"))))
-;; '(font-lock-variable-name-face ((t (:foreground "#afd4d4"))))
-;; '(font-lock-warning-face ((t (:foreground "#dd9999"))))
-;; '(fringe ((nil (:foreground "#aabbcc" :background "#fafafa"))))
-;; '(header-line-highlight ((t (:inherit nil :background "#fabbaa" :foreground "#aaaaaa"))))
-;; '(highlight ((t (:background "#ffdddd"))))
-;; '(ido-only-match ((t (:foreground "#60b770"))))
-;; '(ido-subdir ((t (:foreground "#ff8080"))))
-;; '(isearch ((t (:background "#ddaaee" :foreground "#ffffaa"))))
-;; '(ivy-current-match ((t (:background "#8a99d7" :foreground "white"))))
-;; '(ivy-cursor ((t (:background "#caeade" :foreground "white"))))
-;; '(ivy-minibuffer-match-face-2 ((t (:background "#f9ccda" :foreground "#8a8aba"))))
-;; '(link ((t (:underline "#dadafa" :foreground "#99a9da"))))
-;; '(linum ((t (:foreground "#e8e8e8" :background "#fafafa"))))
-;; '(magit-branch-local ((t (:foreground "#fa88ca"))))
-;; '(magit-branch-remote ((t (:foreground "#89ca88"))))
-;; '(magit-process-ng ((t (:inherit magit-section-heading :foreground "#f090c0"))))
-;; '(magit-process-ok ((t (:inherit magit-section-heading :foreground "#72e090"))))
-;; '(magit-section-heading ((t (:foreground "#f0d270" :weight bold))))
-;; '(magit-section-highlight ((t (:background "#f9f9f9"))))
-;; '(minibuffer-prompt ((t ((:foreground "#fcb2b5")))))
-;; '(mode-line ((t (:background "#faf7f7" :foreground "#d0d0d0"))))
-;; '(mode-line-buffer-id ((t (:background nil :foreground "#90a0e0"))))
-;; '(mode-line-highlight ((t (:background nil :foreground "#f0c0c0"))))
-;; '(mode-line-inactive ((t (:background "#f3f0f0" :foreground "#e0e0e0"))))
-;; '(racket-selfeval-face ((t (:foreground "#cfa9d6"))))
-;; '(rainbow-delimiters-depth-1-face ((t (:foreground "#eeeef8"))))
-;; '(rainbow-delimiters-depth-2-face ((t (:foreground "#e3e3f2"))))
-;; '(rainbow-delimiters-depth-3-face ((t (:foreground "#dedee8"))))
-;; '(rainbow-delimiters-depth-4-face ((t (:foreground "#d8d8e0"))))
-;; '(rainbow-delimiters-depth-5-face ((t (:foreground "#d0d0d8"))))
-;; '(rainbow-delimiters-depth-6-face ((t (:foreground "#c8c8d0"))))
-;; '(rainbow-delimiters-depth-7-face ((t (:foreground "#c0c0c9"))))
-;; '(rainbow-delimiters-depth-8-face ((t (:foreground "#b8b8c0"))))
-;; '(rainbow-delimiters-depth-9-face ((t (:foreground "#a8a8b0"))))
-;; '(rainbow-delimiters-unmatched-face ((t (:foreground "#f4fff8" :background "#ff9999"))))
-;; '(region ((t (:foreground "#f2a3a9" :background "#f2fbf7" :distant-foreground nil))))
-;; '(show-paren-match ((t (:foreground "#f9fff9" :background "#c9cfe9"))))
-;; '(show-paren-mismatch ((t (:background "#aabbea" :foreground "white"))))
-;; '(vertical-border ((t (:foreground "#f8f8f8")))))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
