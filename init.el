a4acd732ff98192280658b6fb6b247da985f7bad
dannyvi <dannyvis@icloud.com>
2019-03-02 08:27:21 +0800

    start window

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '(
+              (tool-bar-lines . 0)
+              (width . 80) ; chars
+              (height . 48) ; lines
+              (background-color . "honeydew")
+              (left . 780)
+              (top . 10)))))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -37,7 +48,9 @@
 
 
 (use-package exec-path-from-shell
-  :config (exec-path-from-shell-initialize))
+  :config
+  (setq exec-path-from-shell-check-startup-files nil)
+  (exec-path-from-shell-initialize))
 
 (use-package evil
   :config
@@ -127,10 +140,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
  '(default ((t (:foreground "#a3a3a3" :background "#fcfcfc" :height 120 :family "Menlo"))))
  '(bold ((t (:foreground "#faaaaf"))))
  '(cursor ((t (:background "#aceab9"))))
+ '(error ((t (:foreground "#f95a7a"))))
  '(font-lock-builtin-face ((t (:foreground "#e0c293"))))
  '(font-lock-comment-delimiter-face ((t (:foreground "#cfcfcf"))))
  '(font-lock-comment-face ((t (:foreground "#d8d8d8"))))
@@ -144,24 +157,42 @@
  '(font-lock-regexp-grouping-construct ((t (:foreground "#aaa999"))))
  '(font-lock-string-face ((t (:foreground "#7fbf8f"))))
  '(font-lock-type-face ((t (:foreground "#99a3e9"))))
- '(font-lock-variable-name-face ((t (:foreground "#80caca"))))
+ '(font-lock-variable-name-face ((t (:foreground "#afd4d4")))) ;"#80caca"
  '(font-lock-warning-face ((t (:foreground "#dd9999"))))
  '(fringe ((nil (:foreground "#aabbcc" :background "#fafafa"))))
  '(header-line-highlight ((t (:inherit nil :background "#fabbaa" :foreground "#aaaaaa"))))
  '(highlight ((t (:background "#ffdddd"))))
- '(isearch ((t (:background "#ddaaee" :foreground "#ffffaa"))))
  '(ido-only-match ((t (:foreground "#60b770"))))
  '(ido-subdir ((t (:foreground "#ff8080"))))
+ '(isearch ((t (:background "#ddaaee" :foreground "#ffffaa"))))
+ '(ivy-current-match ((t (:background "#8a99d7" :foreground "white"))))
+ '(ivy-cursor ((t (:background "#caeade" :foreground "white"))))
+ '(ivy-minibuffer-match-face-2 ((t (:background "#f9ccda" :foreground "#8a8aba"))))
  '(link ((t (:underline "#dadafa" :foreground "#99a9da"))))
  '(linum ((t (:foreground "#e8e8e8" :background "#fafafa"))))
+ '(magit-branch-local ((t (:foreground "#fa88ca"))))
+ '(magit-branch-remote ((t (:foreground "#89ca88"))))
+ '(magit-process-ng ((t (:inherit magit-section-heading :foreground "#f090c0"))))
+ '(magit-process-ok ((t (:inherit magit-section-heading :foreground "#72e090"))))
+ '(magit-section-heading ((t (:foreground "#f0d270" :weight bold))))
+ '(magit-section-highlight ((t (:background "#f9f9f9"))))
  '(minibuffer-prompt ((t ((:foreground "#fcb2b5")))))
  '(mode-line ((t (:background "#faf7f7" :foreground "#d0d0d0"))))
  '(mode-line-buffer-id ((t (:background nil :foreground "#90a0e0"))))
  '(mode-line-highlight ((t (:background nil :foreground "#f0c0c0"))))
  '(mode-line-inactive ((t (:background "#f3f0f0" :foreground "#e0e0e0"))))
+ '(racket-selfeval-face ((t (:foreground "#cfa9d6"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#eeeef8"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#e3e3f2"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#dedee8"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#d8d8e0"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#d0d0d8"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#c8c8d0"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#c0c0c9"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#b8b8c0"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#a8a8b0"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "#f4fff8" :background "#ff9999"))))
  '(region ((t (:foreground "#f2a3a9" :background "#f2fbf7" :distant-foreground nil))))
- '(show-match-paren ((t (:background "#ccaafe" :foreground "#ffeaea"))))
  '(show-paren-match ((t (:foreground "#f9fff9" :background "#c9cfe9"))))
  '(show-paren-mismatch ((t (:background "#aabbea" :foreground "white"))))
- '(vertical-border ((t (:foreground "#f8f8f8"))))
- )
+ '(vertical-border ((t (:foreground "#f8f8f8")))))
